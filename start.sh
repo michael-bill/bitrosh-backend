69ea452d1fb5e063a70550ec5e952d6cd0749d18
Michael B. <biloshitskiy_m@mail.ru>
2025-01-07 18:37:17 +0300

    fix: start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
+
 git pull origin main
-mvn clean package
-nohup java -jar target/backend*.jar > out.txt 2>&1 &
-echo "Application started with PID: $!"
+
+mvn clean install -DskipTests=true
+
+APP_JAR=$(ls target/bitrosh-backend*.jar 2>/dev/null | head -n 1)
+
+if [ -z "$APP_JAR" ]; then
+  echo "No JAR file found after build process."
+  exit 1
+fi
+
+PID=$(pgrep -f "$APP_JAR")
+if [ -n "$PID" ]; then
+  echo "Application already running with PID: $PID. Terminating process."
+  kill "$PID"
+  sleep 2
+  if kill -0 "$PID" >/dev/null 2>&1; then
+    echo "Process did not terminate. Forcing termination."
+    kill -9 "$PID"
+  fi
+fi
+
+nohup java -jar "$APP_JAR" > out.txt 2>&1 &
+
+NEW_PID=$!
+echo "New application started with PID: $NEW_PID"
